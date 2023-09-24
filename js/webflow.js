6e030b3e2885c305b7305d1c5a788bdb43b42086
miguelFernandez98 <139988212+miguelFernandez98@users.noreply.github.com>
2023-09-24 19:33:03 -0400

    update segunda y primera version

--- js/webflow.js
+++ js/webflow.js
@@ -1,12 +1,3 @@
-
-/*!
- * Webflow: Front-end site library
- * @license MIT
- * Inline scripts may access the api using an async handler:
- *   var Webflow = Webflow || [];
- *   Webflow.push(readyFunction);
- */
-
 (() => {
   var __getOwnPropNames = Object.getOwnPropertyNames;
   var __commonJS = (cb, mod) => function __require() {
